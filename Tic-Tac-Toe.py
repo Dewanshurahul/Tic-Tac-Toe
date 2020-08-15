3ff64862eb6a8f7962592a3d160ffd1872b419a8
Dewanshu Rahul4 <dewanshurahul95@gmail.com>
2020-08-15 07:19:54 +0530

    Adding: Toss to Start the Game.

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,5 +1,10 @@
 from random import randint
 print("Fresh start the Game.")
 letter = ['O', 'X']
-player_letter = letter[randint(0,1)]
+player_letter = letter[randint(0, 1)]
 print("Player got", player_letter, "letter")
+toss = randint(0, 1)
+if toss == 0:
+    print("Player gets to Play")
+else:
+    print("Computer gets to Play")
