f016f42d3d3e9cab0c53516c9466aaebbcea132a
Dewanshu Rahul4 <dewanshurahul95@gmail.com>
2020-08-15 08:28:24 +0530

    Adding: Determine whether win or loss or tie

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -4,6 +4,7 @@ letter = ['O', 'X']
 player_letter = letter[randint(0, 1)]
 print("Player got", player_letter, "letter")
 toss = randint(0, 1)
+count = 0  # To count number of Turns
 if toss == 0:
     print("Player gets to Play")
 else:
@@ -24,3 +25,52 @@ def printboard(board):
 
 print("Current Board is :")
 printboard(board)
+
+
+def gamestatus(board):
+    if board['7'] == board['8'] == board['9'] != ' ':  # across the top
+        if player_letter == board['7']:
+            print("Player Wins")
+        else:
+            print("Computer Wins")
+    elif board['4'] == board['5'] == board['6'] != ' ':  # across the middle
+        if player_letter == board['4']:
+            print("Player Wins")
+        else:
+            print("Computer Wins")
+    elif board['1'] == board['2'] == board['3'] != ' ':  # across the bottom
+        if player_letter == board['1']:
+            print("Player Wins")
+        else:
+            print("Computer Wins")
+    elif board['1'] == board['4'] == board['7'] != ' ':  # down the left side
+        if player_letter == board['1']:
+            print("Player Wins")
+        else:
+            print("Computer Wins")
+    elif board['2'] == board['5'] == board['8'] != ' ':  # down the middle
+        if player_letter == board['2']:
+            print("Player Wins")
+        else:
+            print("Computer Wins")
+    elif board['3'] == board['6'] == board['9'] != ' ':  # down the right side
+        if player_letter == board['3']:
+            print("Player Wins")
+        else:
+            print("Computer Wins")
+    elif board['7'] == board['5'] == board['3'] != ' ':  # diagonal
+        if player_letter == board['7']:
+            print("Player Wins")
+        else:
+            print("Computer Wins")
+    elif board['1'] == board['5'] == board['9'] != ' ':  # diagonal
+        if player_letter == board['1']:
+            print("Player Wins")
+        else:
+            print("Computer Wins")
+
+    if count == 9:
+        print("Game Ties.")
+
+
+gamestatus(board)
